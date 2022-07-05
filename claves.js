0c4c42bb1c00c0ef29a0cfb763694cdebe984c5a
DarkGypsy <102004273+DarkGypsy@users.noreply.github.com>
2022-07-05 18:37:38 -0400

    Swordfish III

--- claves.js
+++ claves.js
@@ -1,8 +1,9 @@
 import start from "./index.js";
 function cheats() {
     var cheats = prompt("Ingrese un codigo");
-    if (cheats === "") {
-        console.log("Poner un capitulo")
+    if (cheats === "malvados y asociados") {
+        console.log("Dr. Doofenshmirtz: Si el juego estuviese completo podría mostrarte mi nuevo rayo que hace que todos lean esto con mi voz lo llamo el rayo vocinador");
+        start();
     } else {
         console.log("Codigo invalido, reiniciando el juego...");
        start();
